45da0cc7b185ba66bdce61f83aa709fdcc9a609b
Justin Samuels <biggreenskiing@gmail.com>
2023-06-08 13:18:30 +0530

    some initial test stubs

--- Tests/PSParseTests/PSParseTests.swift
+++ Tests/PSParseTests/PSParseTests.swift
@@ -9,4 +9,27 @@ final class PSParseTests: XCTestCase {
         // Defining Test Cases and Test Methods
         // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
     }
+    func testStandardCell() async throws {
+        
+    }
+    func testCellWithTab() async throws {
+        
+    }
+    func testHeaderLineV1() async throws {
+        
+    }
+    
+    func testFirstDataLineV1() async throws {
+        
+    }
+    
+    func testFirstTwoLinesV1() async throws {
+        
+    }
+    func testItemWithExtraTabV1() async throws {
+        
+    }
+    func testGetLinesV1() async throws {
+        
+    }
 }
