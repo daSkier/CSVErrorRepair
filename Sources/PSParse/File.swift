47fd4ab2abb97bb417e687fdeddf8254c944c5e2
Justin Samuels <biggreenskiing@gmail.com>
2023-09-13 18:21:55 -0600

    add more field validation options and return types

--- Sources/PSParse/File.swift
+++ Sources/PSParse/File.swift
@@ -20,43 +20,69 @@ let dateWithDashesFormatter = {
 }()
 
 enum FieldType: Equatable, Hashable {
-    case integer(expectedValue: Int?, expectedLength: Int?)
+    case integer(nullable: Bool, expectedValue: Int?, expectedLength: Int?)
     case float
-    case string(expectedLength: Int)
+    case string(expectedLength: Int?, startsWith: String?, contains: String?)
     case unknownString
     case date
     case dateTime
 }
 
 extension FieldType {
-    func validate(inputString input: String) -> Bool {
+    func validate(inputString input: String) -> ValidationResult {
         switch self {
-        case .integer(let expectedValue, let expectedLength):
-            if let expectedValue {
+        case .integer(let nullable, let expectedValue, let expectedLength):
+            if input.isEmpty {
+                return nullable ? .null : .invalid
+            }else if let expectedValue {
                 if let inputInt = Int(input) {
-                    return inputInt == expectedValue
+                    return inputInt == expectedValue ? .valid : .invalid
                 }else{
-                    return false
+                    return .invalid
                 }
             }else if let expectedLength {
                 if input.count == expectedLength {
-                    return Int(input) != nil
+                    return Int(input) != nil ? .valid : .invalid
                 }else{
-                    return false
+                    return .invalid
                 }
             } else {
-                return Int(input) != nil
+                return Int(input) != nil ? .valid : .invalid
             }
         case .float:
-            return Float(input) != nil
-        case .string(let expectedLength):
-            return input.count == expectedLength
+            return Float(input) != nil ? .valid : .invalid
+        case .string(let expectedLength, let startsWith, let contains):
+            if let expectedLength {
+                return input.count == expectedLength ? .valid : .invalid
+            }else if let startsWith {
+                if let contains {
+                    return input.hasPrefix(startsWith) && input.contains(contains) ? .valid : .invalid
+                }else{
+                    return input.hasPrefix(startsWith) ? .valid : .invalid
+                }
+            }else if let contains {
+                if input.isEmpty {
+                    return .null
+                }else{
+                    return input.contains(contains) ? .valid : .null
+                }
+            }else {
+                print("failed to find expectedLength or startsWith")
+                return .invalid
+            }
         case .unknownString:
-            return true
+            return .unknownString
         case .date:
-            return dateWithDashesFormatter.date(from: input) != nil
+            return dateWithDashesFormatter.date(from: input) != nil ? .valid : .invalid
         case .dateTime:
-            return dateSpaceTimeFormatter.date(from: input) != nil
+            return dateSpaceTimeFormatter.date(from: input) != nil ? .valid : .invalid
         }
     }
+}
+
+enum ValidationResult {
+    case valid
+    case null
+    case unknownString
+    case invalid
 }
