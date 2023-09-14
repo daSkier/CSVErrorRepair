49dd002801aa1cd74cdb7f9228eaa0d02b7dca2c
Justin Samuels <biggreenskiing@gmail.com>
2023-09-13 18:23:09 -0600

    add ValidationResultSet type

--- Sources/PSParse/LineScanner.swift
+++ Sources/PSParse/LineScanner.swift
@@ -176,3 +176,45 @@ struct LineScanner {
         }
     }
 }
+
+struct ValidationResultSet {
+    var validatedIndicesForward: [Int]
+    var invalidIndiciesForward: [Int]
+    var lessValidatedIndicesForward: [Int]
+    var validatedIndicesBackward: [Int]
+    var invalidIndicesBackward: [Int]
+    var lessValidatedIndicesBackward: [Int]
+    var lastValidForward: Int? { validatedIndicesForward.last }
+    var lastLessValidForward: Int? { lessValidatedIndicesForward.last }
+    var lastValidBackward: Int? { validatedIndicesBackward.last }
+    var lastLessValidBackward: Int? { lessValidatedIndicesBackward.last }
+
+    func validForwardBackDifferenceString() -> String {
+        if let lastValidForward, let lastValidBackward {
+            return "difference between forward/reverse valid indicies \(lastValidBackward-lastValidForward) (\(lastValidForward) vs. \(lastValidBackward)"
+        }else{
+            return "failed to get lastValidForward and/or lastValidBackward"
+        }
+    }
+
+    func lessValidForwardBackDifferenceString() -> String {
+        if let lastLessValidForward, let lastLessValidBackward {
+            return "difference between forward/reverse lessValid indicies \(lastLessValidBackward-lastLessValidForward) (\(lastLessValidForward) vs. \(lastLessValidBackward)"
+        }else{
+            return "failed to get lastLessValidForward and/or lastLessValidBackward"
+        }
+    }
+
+    func mergedLastIndices() -> [Int] {
+        if let lastValidForward, let lastValidBackward, let lastLessValidForward, let lastLessValidBackward {
+            return [
+                lastValidForward,
+                lastLessValidForward,
+                lastValidBackward,
+                lastLessValidBackward
+            ].sorted()
+        }else {
+            return []
+        }
+    }
+}
