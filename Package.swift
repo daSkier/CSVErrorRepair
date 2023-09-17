cf5194681c36bd26ab8e97b2269bcc70498aab92
Justin Samuels <biggreenskiing@gmail.com>
2023-09-16 22:30:46 -0600

    adds CollectionConcurrencyKit

--- Package.swift
+++ Package.swift
@@ -14,11 +14,18 @@ let package = Package(
             name: "PSParse",
             targets: ["PSParse"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/JohnSundell/CollectionConcurrencyKit.git", from: "0.1.0"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "PSParse"),
+            name: "PSParse",
+            dependencies: [
+                .product(name: "CollectionConcurrencyKit", package: "CollectionConcurrencyKit"),
+            ]
+        ),
         .testTarget(
             name: "PSParseTests",
             dependencies: ["PSParse"]),
