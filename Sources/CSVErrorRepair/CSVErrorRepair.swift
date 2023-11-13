b5fd535f5a65563252338e2a334c0253c40eec67
Justin Samuels <biggreenskiing@gmail.com>
2023-11-12 21:58:44 -0700

    move ValidationResultSet to own file

--- Sources/CSVErrorRepair/CSVErrorRepair.swift
+++ Sources/CSVErrorRepair/CSVErrorRepair.swift
@@ -414,60 +414,6 @@ enum ParseError: Error {
     case failedToGetStringFromData
 }
 
-struct ValidationResultSet {
-    var validatedIndicesForward: [Int]
-    var invalidIndiciesForward: [Int]
-    var lessValidatedIndicesForward: [Int]
-    var validatedIndicesBackward: [Int]
-    var invalidIndicesBackward: [Int]
-    var lessValidatedIndicesBackward: [Int]
-    var lastValidForward: Int? { validatedIndicesForward.last }
-    var lastLessValidForward: Int? { lessValidatedIndicesForward.last }
-    var lastValidBackward: Int? { validatedIndicesBackward.last }
-    var lastLessValidBackward: Int? { lessValidatedIndicesBackward.last }
-
-    func validForwardBackDifferenceString() -> String {
-        if let lastValidForward, let lastValidBackward {
-            return "difference between forward/reverse valid indicies \(lastValidBackward-lastValidForward) (\(lastValidForward) vs. \(lastValidBackward)"
-        }else{
-            return "failed to get lastValidForward and/or lastValidBackward"
-        }
-    }
-
-    func lessValidForwardBackDifferenceString() -> String {
-        if let lastLessValidForward, let lastLessValidBackward {
-            return "difference between forward/reverse lessValid indicies \(lastLessValidBackward-lastLessValidForward) (\(lastLessValidForward) vs. \(lastLessValidBackward)"
-        }else{
-            return "failed to get lastLessValidForward and/or lastLessValidBackward"
-        }
-    }
-
-    func mergedLastIndices() throws -> [Int] {
-        if let lastValidForward, let lastValidBackward, let lastLessValidForward, let lastLessValidBackward {
-            return [
-                lastValidForward,
-                lastLessValidForward,
-                lastValidBackward,
-                lastLessValidBackward
-            ].sorted()
-        }else {
-            print("failed to get mergedLastIndicies - lastValidForward: \(String(describing: lastValidForward)) / lastValidBackward: \(String(describing: lastValidBackward)) / lastLessValidForward: \(String(describing: lastLessValidForward)) / lastLessValidBackward: \(String(describing: lastLessValidBackward))")
-            throw ValidationResultSetError.oneLastIndicyNil
-        }
-    }
-
-    func printResults() {
-        print(validForwardBackDifferenceString())
-        print(lessValidForwardBackDifferenceString())
-        print("invalidIndicesForward: \(invalidIndiciesForward)")
-        print("invalidIndicesBackward: \(invalidIndicesBackward)")
-        print("valid indicies array: \(String(describing: try? mergedLastIndices()))")
-    }
-    enum ValidationResultSetError: Error {
-        case oneLastIndicyNil
-    }
-}
-
 struct CSVFileIssues {
     var fileUrl: URL
     var issues: [CSVLineIssue]
