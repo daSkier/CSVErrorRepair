be7beb3d337f1130d03e2781585768084a3f66ed
Justin Samuels <biggreenskiing@gmail.com>
2024-01-06 10:36:24 -0700

    adds non-inout variant of correctErrorsIn()

--- Sources/CSVErrorRepair/CSVErrorRepair.swift
+++ Sources/CSVErrorRepair/CSVErrorRepair.swift
@@ -419,6 +419,43 @@ public struct CSVErrorRepair {
 //        }
 //    }
     #endif
+
+    public static func correctErrorsIn(_ inputLines: [[String]], forUrl url: URL, fieldTypes: [String : FieldType]) async -> (resultLines: [[String]], issues: FileIssues) {
+        return await Task {
+            var lines = inputLines
+            let linesWithIssues = CSVErrorRepair.findLinesWithIncorrectElementCount(fromLines: lines)
+            if linesWithIssues.count > 0 {
+                for issueLine in linesWithIssues {
+                    CSVErrorRepair.repairSequentialShortLines(lines: &lines,
+                                                  firstLineIndex: issueLine.lineIndex,
+                                                  targetColumnCount: issueLine.expectedColumnCount)
+                }
+                lines.removeAll{ $0.count == 0 } // prevents issues with lines that end with /r
+                lines.removeAll { $0.count == 1 && $0.first!.isEmpty } // prevents issues with lines that end with /r
+                let linesWithIssuesAfterSequentialLineRepair = CSVErrorRepair.findLinesWithIncorrectElementCount(fromLines: lines)
+                let fieldTypes = lines.first!.map { fieldName in
+                    guard let type = fieldTypes[fieldName] else {
+                        fatalError("failed to get field type for \(fieldName)")
+                    }
+                    return type
+                }
+                for issueLine in linesWithIssuesAfterSequentialLineRepair {
+                    CSVErrorRepair.repairLinesWithMoreColumnsBasedOnExpectedFields(forLine: &lines[issueLine.lineIndex],
+                                                                                    targetColumnCount: issueLine.expectedColumnCount,
+                                                                                    expectedFieldTypes: fieldTypes,
+                                                                                    fileName: url.lastPathComponent,
+                                                                                    lineNumber: issueLine.lineIndex)
+                }
+                let linesWithIssuesAfterLongLineRepair = CSVErrorRepair.findLinesWithIncorrectElementCount(fromLines: lines)
+                if !linesWithIssuesAfterLongLineRepair.isEmpty {
+                    print("linesWithIssuesAfterLongLineRepair: \(linesWithIssuesAfterLongLineRepair)")
+                }
+                return (lines, FileIssues(fileUrl: url, issues: linesWithIssuesAfterLongLineRepair))
+            }else{
+                return (lines, FileIssues(fileUrl: url, issues: linesWithIssues))
+            }
+        }.value
+    }
 }
 
 public enum ParseError: Error {
