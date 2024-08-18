e27ca7d2c353ad3198314fc46191afcda3777c0c
Justin Samuels <biggreenskiing@gmail.com>
2024-08-17 22:49:05 -0600

    updates to v6 and v14

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "CSVErrorRepair",
     platforms: [
-       .macOS(.v13)
+       .macOS(.v14)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
@@ -29,5 +29,6 @@ let package = Package(
         .testTarget(
             name: "CSVErrorRepairTests",
             dependencies: ["CSVErrorRepair"]),
-    ]
+    ],
+    swiftLanguageModes: [.v6]
 )
